cb21855909bf8f7083aa344aa43b147bf44c28a6
x86_64 <x86mail@gmail.com>
2012-07-21 13:11:10 +0400

    Fix build_error script

--- src/errors_list.c
+++ src/errors_list.c
@@ -1,15 +1,14 @@
 
 static err_item errs_list[] = {
- { -327, "src/main_memcache.c: memcache_delete error" },
- { -309, "src/main_memcache.c: memcache_set error" },
- { -224, "src/main_memcache.c: no action specified" },
- { -197, "src/main_memcache.c: memcache_value_unserialize unknown output datatype: pass output key or set value_type in configuration" },
- { -191, "src/main_memcache.c: memcache_value_unserialize wrong output data supplied" },
- { -155, "src/main_memcache.c: memcache configuration error: wrong management modes configuration" },
- { -149, "src/main_memcache.c: invalid config specified" },
- { -107, "src/main_memcache.c: calloc failed" },
+ { -527, "src/main_memcache.c: memcache_delete error" },
+ { -509, "src/main_memcache.c: memcache_set error" },
+ { -424, "src/main_memcache.c: no action specified" },
+ { -397, "src/main_memcache.c: memcache_value_unserialize unknown output datatype: pass output key or set value_type in configuration" },
+ { -391, "src/main_memcache.c: memcache_value_unserialize wrong output data supplied" },
+ { -355, "src/main_memcache.c: memcache configuration error: wrong management modes configuration" },
+ { -349, "src/main_memcache.c: invalid config specified" },
+ { -307, "src/main_memcache.c: calloc failed" },
 
-	{ 0, NULL }
 };
 #define            errs_list_size      sizeof(errs_list[0])
 #define            errs_list_nelements sizeof(errs_list) / errs_list_size
